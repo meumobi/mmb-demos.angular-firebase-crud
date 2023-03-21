dfccadd02dff128852cb4e47263346fb1bb39e08
vdias38 <victor.mdias.38@gmail.com>
2023-03-21 23:28:17 +0000

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'mmbd-angular-firebase-crud',
+    appId: '1:249445234442:web:061dfed3284af896b0acf3',
+    storageBucket: 'mmbd-angular-firebase-crud.appspot.com',
+    apiKey: 'AIzaSyAtOkblzMo-MMSI9mV67Tc4f5yq1jiNEvw',
+    authDomain: 'mmbd-angular-firebase-crud.firebaseapp.com',
+    messagingSenderId: '249445234442',
+  },};
