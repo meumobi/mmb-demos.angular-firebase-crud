7c852b5f1063e5c6764b546377c0adc9295cdfe6
vdias38 <victor.mdias.38@gmail.com>
2023-12-28 21:33:21 +0000

    1.3-SETUP shared library

--- projects/angular-crud/src/app/app.component.ts
+++ projects/angular-crud/src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Profile } from '@meumobi/shared';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-crud';
+
+  ngOnInit(): void {
+    new Profile();
+  }
 }
